7ddd8e938377f53c32cc51ad8fcec52ff7c59322
Branden Robinson <ifwhyhow@gmail.com>
2025-04-15 12:26:01 -0400

    Remake the project with Blazor Hybrid and .NET MAUI

---
+++ TheNextLevel/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TheNextLevel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

6ef5c28ebff2e7da8f2ed0c1c76db89bf90d2ab1
seeker-dev <bresonate@gmail.com>
2025-06-10 16:54:46 -0400

    More cleaning up and more v2ing

---
+++ TheNextLevel/ViewModels/NLTask.cs
@@ -0,0 +1,11 @@
+﻿namespace TheNextLevel.ViewModels
+{
+    public class NLTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Priority { get; set; } = "Normal"; // Default priority
+        public bool IsCompleted { get; set; } = false;
+    }
+}

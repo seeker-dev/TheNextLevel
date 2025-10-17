195537ff164e6a1d500dd89e625171ec9ab0b982
seeker-dev <bresonate@gmail.com>
2025-10-16 21:46:17 -0400

    Prepare for hybrid pagination, UI fixes, new Tasks page

--- TheNextLevel/Core/Interfaces/IProjectRepository.cs
+++ TheNextLevel/Core/Interfaces/IProjectRepository.cs
@@ -4,8 +4,10 @@ namespace TheNextLevel.Core.Interfaces;
 
 public interface IProjectRepository
 {
-    Task<IEnumerable<Project>> GetAllAsync();
+    Task<IEnumerable<Project>> GetAllAsync(bool includeTasks = false);
     Task<Project?> GetByIdAsync(int id);
+    Task<IEnumerable<Project>> GetAsync(int startIndex, int count, bool includeTasks = false);
+    Task<int> GetTotalProjectsCountAsync(); 
     Task<Project> AddAsync(Project project);
     Task<Project> UpdateAsync(Project project);
     Task<bool> DeleteAsync(int id);

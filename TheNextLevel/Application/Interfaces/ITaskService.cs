994880a859d6f5a738cb900754bc8876454c0669
seeker-dev <bresonate@gmail.com>
2026-01-19 14:07:30 -0500

    TNL-117 - Remove unnecessary methods from ITaskService

--- TheNextLevel/Application/Interfaces/ITaskService.cs
+++ TheNextLevel/Application/Interfaces/ITaskService.cs
@@ -5,20 +5,16 @@ namespace TheNextLevel.Application.Interfaces;
 
 public interface ITaskService
 {
-    System.Threading.Tasks.Task<IEnumerable<TaskDto>> GetAllTasksAsync();
     System.Threading.Tasks.Task<TaskDto?> GetTaskByIdAsync(int id);
+    System.Threading.Tasks.Task<PagedResult<TaskDto>> GetTasksPagedAsync(int skip, int take, bool isCompleted = false);
+    System.Threading.Tasks.Task<PagedResult<TaskDto>> GetTasksByProjectPagedAsync(int projectId, int skip, int take, bool isCompleted = false);
+    System.Threading.Tasks.Task<IEnumerable<TaskDto>> GetUngroupedTasksAsync();
     System.Threading.Tasks.Task<int> CreateTaskAsync(CreateTaskRequest request);
     System.Threading.Tasks.Task<bool> UpdateTaskAsync(int id, UpdateTaskRequest request);
     System.Threading.Tasks.Task<bool> DeleteTaskAsync(int id);
     System.Threading.Tasks.Task<bool> CompleteTaskAsync(int id);
     System.Threading.Tasks.Task<bool> ReopenTaskAsync(int id);
-    System.Threading.Tasks.Task<IEnumerable<TaskDto>> GetTasksByStatusAsync(bool isCompleted);
-    System.Threading.Tasks.Task<IEnumerable<TaskDto>> GetTasksByProjectAsync(int projectId);
-    System.Threading.Tasks.Task<PagedResult<TaskDto>> GetTasksByProjectPagedAsync(int projectId, int skip, int take, bool isCompleted = false);
-    System.Threading.Tasks.Task<IEnumerable<TaskDto>> GetUngroupedTasksAsync();
     System.Threading.Tasks.Task<bool> AssignTaskToProjectAsync(int taskId, int? projectId);
-    System.Threading.Tasks.Task<PagedResult<TaskDto>> GetTasksPagedAsync(int skip, int take, bool isCompleted = false);
     System.Threading.Tasks.Task<int> CreateSubtaskAsync(CreateSubtaskRequest request);
     System.Threading.Tasks.Task<PagedResult<TaskDto>> GetSubtasksByParentIdAsync(int parentTaskId, int skip, int take);
-    System.Threading.Tasks.Task<bool> CanTaskHaveSubtasksAsync(int taskId);
 }

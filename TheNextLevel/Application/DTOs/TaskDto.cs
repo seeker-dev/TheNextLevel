f55f538808350f93dd67cfe281f99e8d856c974b
seeker-dev <bresonate@gmail.com>
2026-03-14 16:57:59 -0400

    TNL-171 - No semblance of IsCompleted in the UI

--- TheNextLevel/Application/DTOs/TaskDto.cs
+++ TheNextLevel/Application/DTOs/TaskDto.cs
@@ -3,10 +3,10 @@ namespace TheNextLevel.Application.DTOs;
 public record TaskDto(int Id, string Name, string Description, bool IsCompleted, TaskState Status, int? ProjectId = null, int? ParentTaskId = null) : IItemDto;
 
 public record CreateTaskRequest(int ProjectId, string Name, string Description);
-public record UpdateTaskRequest(string Name, string Description, bool IsCompleted, int ProjectId);
+public record UpdateTaskRequest(string Name, string Description, int ProjectId);
 
 public record CreateSubtaskRequest(int ParentTaskId, string Name, string Description);
-public record UpdateSubtaskRequest(string Name, string Description, bool IsCompleted, int ParentTaskId);
+public record UpdateSubtaskRequest(string Name, string Description, int ParentTaskId);
 
 public enum TaskState
 {
